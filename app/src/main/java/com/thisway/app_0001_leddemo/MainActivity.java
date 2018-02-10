764f15e63dd16b648019c225b4bb13c2d602b59d
Terey001 <447647803@qq.com>
2018-02-10 21:13:25 +0800

    V1

---
+++ app/src/main/java/com/thisway/app_0001_leddemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thisway.app_0001_leddemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
